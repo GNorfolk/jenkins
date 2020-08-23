18ab32ca384da9c4e687703245ba337deb969f69
George Norfolk <george.norfolk@lexisnexis.co.uk>
2020-08-23 23:47:38 +0100

    Adding packer script to install jenkins and use that ami.

---
+++ terraform/modules/main/data.tf
@@ -0,0 +1,7 @@
+data "aws_ami" "jenkins" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["jenkins*"]
+  }
+}
--- terraform/modules/main/ec2.tf
+++ terraform/modules/main/ec2.tf
@@ -18,9 +18,32 @@ resource "aws_security_group" "packer" {
   }
 }
 
+resource "aws_security_group" "jenkins" {
+  name          = "jenkins"
+  vpc_id        = aws_vpc.main.id
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "jenkins"
+  }
+}
+
 resource "aws_instance" "jenkins" {
-  ami           = "ami-07d9160fa81ccffb5"
+  ami           = data.aws_ami.jenkins.id
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.main.id
   iam_instance_profile = aws_iam_role.jenkins.name
+  tags = {
+    Name = "jenkins"
+  }
 }
